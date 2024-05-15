08457fb436fe63d98f13ded48468cb50283366ff
GavScoLov <110779396+GavScoLov@users.noreply.github.com>
2024-05-15 13:59:41 -0500

    Commit Test Scanner Header File

---
+++ testScanner.h
@@ -0,0 +1,26 @@
+// Gavin Love - CS4280 - Test Scanner Header File
+#ifndef TESTSCANNER_H
+#define TESTSCANNER_H
+
+#include <iostream>
+#include <string>
+
+#include "node.h"
+
+using namespace std;
+
+const int MAX = 100;
+
+//typedef struct {
+//	node_t *root;
+//} tree_t;
+
+//extern char fileArr[MAX];
+//extern char fileWord[MAX];
+//extern char capitalLtr[MAX];   // Array of Capital Letters
+//extern string words[MAX];      // Array of Words from File
+//extern string nodeArr[MAX];    // Array of Nodes that contain '<CapitalLetters>: <Words>'
+
+void testScanner(char*, char*, int);
+
+#endif
